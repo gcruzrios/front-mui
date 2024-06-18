3b8ca0681c7c5fb00234a08e7f39fa1e5fdd652b
gcruzrios <gcruzr@gmail.com>
2024-06-18 13:29:25 -0600

    App working con CRUD Complete 100% con Background

--- src/pages/SignInSide.js
+++ src/pages/SignInSide.js
@@ -116,7 +116,7 @@ export default function SignInSide() {
           sm={4}
           md={9}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundImage: 'url(https://mui.greiv.in/background.jpg)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
